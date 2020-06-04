9a72c609822d7ca93ebec95d887fae5e6afff8e2
meetparikh12 <parikhmeet1234@gmail.com>
2020-06-04 21:00:26 +0530

    Completed booking table section for landing page

--- resto-dine-frontend/src/components/Reservation.js
+++ resto-dine-frontend/src/components/Reservation.js
@@ -5,115 +5,81 @@ export default function Reservation() {
         <div className="booking_table">
         <br/>
             <div className="container">
-                    <div className="section-header mb-5">
-                        <h2>Book Your Table</h2>
-                        <div className="asset">
-                            <span></span>
-                            <img src="/img/assets/spoon.png" alt="Spoon icon"/>
-                            <span></span>
-                        </div>
+                <div className="section-header mb-3">
+                    <h2>Book Your Table</h2>
+                    <div className="asset">
+                        <span></span>
+                        <img src="/img/assets/spoon.png" alt="Spoon icon"/>
+                        <span></span>
                     </div>
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="table_title text-center">
-                                    <span class="title text-center"><i>Reservation</i></span>
-                                    <h3 class="book-table-title text-center text-uppercase m-b-35 m-t-2">Book table</h3>
-                                </div>
-                                <form className="probootstrap-form">
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <div className="row">
-                                            <div className="form-group">
-                                                <label for="people">How Many People</label>
-                                                <div className="form-field">
-                                                <select
-                                                    name="people"
-                                                    className="form-control"
-                                                >
-                                                    <option value="#">1 people</option>
-                                                    <option value="#">2 people</option>
-                                                    <option value="#">3 people</option>
-                                                    <option value="#">4+ people</option>
-                                                </select>
-                                                </div>
-                                            </div>
-                                            </div>
-                                            <div className="row">
-
-                                            <div className="form-group">
-                                                <label for="date">Date</label>
-                                                <div className="form-field">
-                                                <input
-                                                    type="date"
-                                                    name="date"
-                                                    className="form-control"
-                                                ></input>
-                                                </div>
-                                            </div>
-                                            </div>
-                                            <div className="row">
-                                            <div className="form-group">
-                                                <label for="time">Time</label>
-                                                <div className="form-field">
-                                                <input
-                                                    type="time"
-                                                    name="time"
-                                                    className="form-control"
-                                                ></input>
-                                                </div>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="row">
-                                            <div className="form-group">
-                                                <label for="name">Name</label>
-                                                <div className="form-field">
-                                                <input
-                                                    type="text"
-                                                    name="name"
-                                                    className="form-control"
-                                                    placeholder="Your full name"
-                                                ></input>
-                                                </div>
-                                            </div>
-                                            </div>
-                                        
-                                            <div className="row">
-                                            <div className="form-group">
-                                                <label for="email">Email</label>
-                                                <div className="form-field">
-                                                <input
-                                                    type="text"
-                                                    name="email"
-                                                    className="form-control"
-                                                    placeholder="Your email address"
-                                                ></input>
-                                                </div>
-                                            </div>
-                                            </div>
-                                            <div className="row">
-                                            <div className="form-group">
-                                                <label for="phone">Phone</label>
-                                                <div className="form-field">
-                                                <input
-                                                    type="text"
-                                                    name="phone"
-                                                    className="form-control"
-                                                    placeholder="Your phone"
-                                                ></input>
-                                                </div>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>    
-                            </div>
-                            <div className="col-md-6">
-                                <img src ="https://previews.customer.envatousercontent.com/files/220083862/Inline%20Preview%20Image.jpg"/>
-                            </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-md-12">
+                        <div className="table_title text-center">
+                            <h2 className="text-center"><i>Reservation</i></h2>
                         </div>
                     </div>
                 </div>
+                <form className="mb-5">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <span className="field-name" >How Many People ?</span>
+                                <div className="form-field mt-2">
+                                    <select name="people" className="form-control">
+                                        <option value="#">1 people</option>
+                                        <option value="#">2 people</option>
+                                        <option value="#">3 people</option>
+                                        <option value="#">4+ people</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">  
+                            <div className="form-group">
+                                <span className="field-name">Date :</span>
+                                <div className="form-field mt-2">
+                                    <input type="date" name="date" className="form-control"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <span className="field-name">Time :</span>
+                                <div className="form-field mt-2">
+                                    <input type="time" name="time" className="form-control"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <span className="field-name">Name :</span>
+                                <div className="form-field mt-2">
+                                    <input type="text" name="name" className="form-control" placeholder="Your full name"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <span className="field-name">Email :</span>
+                                <div className="form-field mt-2">
+                                <input type="text" name="email" className="form-control" placeholder="Your email address"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <span className="field-name">Phone :</span>
+                                <div className="form-field mt-2">
+                                <input type="text" name="phone" className="form-control" placeholder="Your phone"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>    
+            </div>
+        </div>
     )
 }
