b0eacfd7711163f1f0de5465e49b1f61e6324701
meetparikh12 <parikhmeet1234@gmail.com>
2020-06-07 13:38:42 +0530

    Completed designing for FoodProduct page

--- resto-dine-frontend/src/components/FoodItem.js
+++ resto-dine-frontend/src/components/FoodItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import './FoodItem.css'
+import { Link } from 'react-router-dom'
 export default function FoodItem(props) {
     return (
         <div className="col-md-6 col-lg-4 product_item">
             <div className="card mb-4" style={{"width": "100%"}}>
-                <img className="card-img-top" src={props.image} style={{height: "14.5rem", borderBottom: "1px solid rgba(0,0,0,.1)"}} alt="Food-Post"/>
+                <div className="view overlay hm-black-light">
+                    <img className="card-img-top food-img-hover" src={props.image} style={{height: "14.5rem", borderBottom: "1px solid rgba(0,0,0,.1)"}} alt="Food-Post"/>
+                    <div className="mask"></div>
+                    <Link><button className="btn">View</button></Link>
+                </div>
                 <div className="card-body">
                     <h5 className="text-center font-weight-light card-title">{props.name}</h5>
                     <p className="card-text font-weight-bold text-center text-danger">{props.price}/- INR</p>
