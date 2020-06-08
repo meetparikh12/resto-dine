77e199932b2dbc2548d07c66bff82c8eb1aadd68
meetparikh12 <parikhmeet1234@gmail.com>
2020-06-08 16:50:45 +0530

    Added redux store to manage application state

---
+++ resto-dine-frontend/src/reducers/cartReducer.js
@@ -0,0 +1,21 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const initialState = {
+    cartProduct : []
+}
+
+export const cartReducer = (state=initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_TO_CART:
+            let product = [...state.product, action.payload]
+            return {
+                ...state,
+                cartProduct: product
+            }
+    
+        default:
+            return {
+                ...state
+            }
+    }
+}
